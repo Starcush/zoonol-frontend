708feb6240d7b83fbad8839de755bd3058a67908
starcush <hacker0419@gmail.com>
2023-08-13 13:11:35 -0800

    bottom sheet 틀 제작

--- src/components/store/StoreInfoWindowMobile.js
+++ src/components/store/StoreInfoWindowMobile.js
@@ -4,12 +4,28 @@ import { motion } from 'framer-motion';
 
 const StoreInfoWindowMobile = () => {
   return (
-    <Wrapper drag="y" dragConstraints={{ top: 0, bottom: 300 }}>
+    <Wrapper
+      drag="y"
+      dragConstraints={{
+        top: (-1 * window.innerHeight) / 2 + 80,
+        bottom: window.innerHeight / 2 - 80,
+      }}
+    >
       Title
     </Wrapper>
   );
 };
 
-const Wrapper = styled(motion.div)``;
+const Wrapper = styled(motion.div)`
+  position: absolute;
+  top: 50%;
+  right: 0;
+  background-color: #ffffff;
+  width: 100%;
+  height: 200%;
+  z-index: 1;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+`;
 
 export default StoreInfoWindowMobile;
