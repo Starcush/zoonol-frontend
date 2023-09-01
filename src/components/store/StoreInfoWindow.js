53e174edc14e7d6efc0ac30dd393d7f7e57c568a
starcush <hacker0419@gmail.com>
2023-09-01 13:20:24 -0800

    revert update codes

--- src/components/store/StoreInfoWindow.js
+++ src/components/store/StoreInfoWindow.js
@@ -79,7 +79,7 @@ const StoreInfoWindow = ({ store, closeInfoWindow }) => {
     return (
       <Fragment>
         <StoreInfoWrapper>
-          <Name>{name} *</Name>
+          <Name>{name}</Name>
           <Category>{category}</Category>
         </StoreInfoWrapper>
         {isZoonolPlace && (
@@ -210,8 +210,7 @@ const MobileWrapper = styled(motion.div)`
 
 const DesktopWrapper = styled.div`
   padding: 20px;
-  /* background-color: white; */
-  background-color: green;
+  background-color: white;
   border-radius: 6px;
   box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.3);
   position: fixed;
