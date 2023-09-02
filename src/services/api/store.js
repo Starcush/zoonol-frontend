5e75c391fa53a261edf11936ef1b9cbc80de0f5b
starcush <hacker0419@gmail.com>
2023-09-01 17:07:01 -0800

    주석 제거

--- src/services/api/store.js
+++ src/services/api/store.js
@@ -2,7 +2,6 @@ import { defaultAxios } from '@/lib/axios';
 
 const storeService = {
   async getStoreList() {
-    console.log('defaultAxios: ', defaultAxios);
     const { data } = await defaultAxios.get('/store/list');
     return data;
   },
